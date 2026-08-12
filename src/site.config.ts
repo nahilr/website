9d32e2fe1e00ec71a8b78e876267945bd9f17e01
nahilrasheed <88521572+nahilr@users.noreply.github.com>
2026-08-13 00:38:21 +0530

    fix: add thm badge, update headings and content in about and projects pages, and adjust site navigation

--- src/site.config.ts
+++ src/site.config.ts
@@ -58,7 +58,7 @@ export const theme: ThemeUserConfig = {
       { title: 'Vault', link: '/vault' },
       { title: 'Archive', link: '/archives' },
       { title: 'Projects', link: '/projects' },
-      { title: 'whoami', link: '/about' }
+      { title: 'About', link: '/about' }
     ]
   },
 
